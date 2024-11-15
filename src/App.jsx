6a68757b5d625e2d20ecd20766f2d557dd961c89
Dilshodbek8 <20020905xd@gmail.com>
2024-11-15 17:42:30 +0500

    footer boldi

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ export default function App() {
     <div>
       <h1> logo</h1>
       <h2>content</h2>
+      <div className="flex bg-gray-500 text-white p-10 text-5xl">
+        <h1>footer</h1>
+      </div>
     </div>
   );
 }
