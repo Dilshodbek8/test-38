0fd93a63a08fe5c6fa18a33291e8b11653103eed
Dilshodbek8 <20020905xd@gmail.com>
2024-11-15 17:44:44 +0500

    navbar changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import { Typography, Card } from "@material-tailwind/react";
 export default function App() {
   return (
     <div>
-      <h1> logo</h1>
+      <div className="flex p-5 bg-blue-600 gap-20">
+        <h1> logo</h1>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>service</li>
+        </ul>
+      </div>
       <h2>content</h2>
       <div className="flex bg-gray-500 text-white p-10 text-5xl">
         <h1>footer</h1>
