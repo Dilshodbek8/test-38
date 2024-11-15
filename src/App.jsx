8b91fb46bfbd8de1e9ce3a093b7265045a7fa296
Dilshodbek8 <20020905xd@gmail.com>
2024-11-15 17:39:35 +0500

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Typography, Card } from "@material-tailwind/react";
+
+export default function App() {
+  return (
+    <div>
+      <h1> logo</h1>
+      <h2>content</h2>
+    </div>
+  );
+}
